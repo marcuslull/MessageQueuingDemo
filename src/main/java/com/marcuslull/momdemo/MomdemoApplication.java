8a8a2a95cceb58e3e542cd97d20f58bce3a4bd97
Marcus Lull <marcuslull@gmail.com>
2024-01-18 09:36:39 -0800

    initial commit

---
+++ src/main/java/com/marcuslull/momdemo/MomdemoApplication.java
@@ -0,0 +1,13 @@
+package com.marcuslull.momdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MomdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marcuslull/momdemo/MomdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcuslull.momdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
