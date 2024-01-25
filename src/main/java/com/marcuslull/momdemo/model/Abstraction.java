9c55071257c968aa2a334b8243f72690299102cd
Marcus Lull <marcuslull@gmail.com>
2024-01-25 13:27:20 -0800

    prototyping model classes

--- src/main/java/com/marcuslull/momdemo/model/Abstraction.java
+++ src/main/java/com/marcuslull/momdemo/model/Abstraction.java
@@ -1,4 +1,23 @@
 package com.marcuslull.momdemo.model;
 
-public class Abstraction extends Resource{
+import com.marcuslull.momdemo.model.records.ResourceRecord;
+
+// Description provided by: https://bard.google.com/
+public class Abstraction extends Resource {
+    private final Simulation simulation;
+    private final String name = "Abstraction";
+    private final String description = "Abstraction layers civilization like a skyscraper, " +
+            "lifting us from the ground of basic needs to soar among the clouds of innovation, " +
+            "each level built on the invisible foundations of knowledge and trust.";
+    private int productionTime; // difficulty * rarity * production * tech level / current tech level
+
+    public Abstraction(Simulation simulation, ResourceRecord record) {
+        this.simulation = simulation;
+        this.rarity = record.rarity();
+        this.difficulty = record.difficulty();
+        this.production = record.production();
+        this.techLevel = record.techLevel();
+        this.productionTime = (this.difficulty.ordinal() * this.rarity.ordinal() * this.production.ordinal() *
+                this.techLevel.ordinal()) / simulation.getCurrentTechLevel().ordinal();
+    }
 }
