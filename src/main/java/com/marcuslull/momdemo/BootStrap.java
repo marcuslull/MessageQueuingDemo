0257abbd339ebb7e2269804b00753a0b2677fcf8
Marcus Lull <marcuslull@gmail.com>
2024-01-25 13:43:06 -0800

    adding work model class and desecription for education

--- src/main/java/com/marcuslull/momdemo/BootStrap.java
+++ src/main/java/com/marcuslull/momdemo/BootStrap.java
@@ -16,7 +16,7 @@ public class BootStrap implements CommandLineRunner {
     @Override
     public void run(String... args) {
 
-        ceramicProducer.produce(3);
-        ceramicProducer.produce(5);
+//        ceramicProducer.produce(3);
+//        ceramicProducer.produce(5);
     }
 }
